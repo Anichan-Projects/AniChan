63caf8d168a8139ad2e9486141b8ae278d1b1852
Hiyurin <nguyenhuyentran03122004@gmail.com>
2024-04-08 02:08:06 +0700

    Update: Add new command 'help' to display a list of available commands

---
+++ src/commands/others/help.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
+const path = require('path');
+const language = require('./../../language/language_setup.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription(`${language.__n(`help.command_description`)}`),
+  async execute(interaction) {
+    try {
+      const embed = new MessageEmbed()
+        .setTitle(`${language.__n(`help.command_title`)}`)
+        .setDescription(`${language.__n(`help.embed_description`)}`)
+        .setTimestamp();
+        const commandsDirectory = path.join(__dirname, '..');
+        const commandFolders = fs.readdirSync(commandsDirectory).filter(file => fs.statSync(path.join(commandsDirectory, file)).isDirectory());
+        for (const folder of commandFolders) {
+            const folderPath = path.join(commandsDirectory, folder);
+            const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const filePath = path.join(folderPath, file);
+                const command = require(filePath);
+                embed.addFields({ name: command.data.name, value: command.data.description });
+            }
+        }
+        interaction.reply({ embeds: [embed] });
+    } catch (error) {
+        console.error(`${language.__n(`global.error`)}`, error);
+        interaction.reply(`${language.__n(`global.error_reply`)}`);
+    }
+  },
+};
